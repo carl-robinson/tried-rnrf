e2486d6e78d7ded225cfc665d879a9fed1343e3f
Carl <carl.robinson@gmail.com>
2017-10-05 20:39:26 +0200

    optional tp 1.1

---
+++ fac_tp_1_1.py
@@ -0,0 +1,10 @@
+%matplotlib inline
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import random
+from scipy import stats
+
+input_file = open('donnees.txt')
+for line in input_file:
+    print(line)
